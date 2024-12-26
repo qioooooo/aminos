71396a304a0b203d1f32a61d1ad72e4554478c66
qioooooo <2310062162@qq.com>
2024-12-26 21:17:51 +0800

    Add files via upload

---
+++ Vsa/LoaderAPI.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Microsoft.JScript.Vsa
+{
+	// Token: 0x0200013A RID: 314
+	internal enum LoaderAPI
+	{
+		// Token: 0x04000791 RID: 1937
+		LoadFrom,
+		// Token: 0x04000792 RID: 1938
+		LoadFile,
+		// Token: 0x04000793 RID: 1939
+		ReflectionOnlyLoadFrom
+	}
+}
